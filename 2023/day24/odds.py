3bd9aa262522d68297915429aaf611d5faf1f581
Yue Peng <yuepaang@gmail.com>
2023-12-26 00:17:43 +0800

    plane

--- 2023/day24/odds.py
+++ 2023/day24/odds.py
@@ -46,6 +46,36 @@ def extract_intersection(line1, line2):
     return (x, y)
 
 
+def extract_plane(hailstone):
+    p0 = (hailstone.x, hailstone.y, hailstone.z)
+    p1 = (
+        hailstone.x + hailstone.vx,
+        hailstone.y + hailstone.vy,
+        hailstone.z + hailstone.vz,
+    )
+    p2 = (
+        hailstone.x + hailstone.vx + hailstone.vx,
+        hailstone.y + hailstone.vy + hailstone.vy,
+        hailstone.z + hailstone.vz + hailstone.vz,
+    )
+    print(p0, p1, p2)
+    a1 = p1[0] - p0[0]
+    b1 = p1[1] - p0[1]
+    c1 = p1[2] - p0[2]
+    a2 = p2[0] - p0[0]
+    b2 = p2[1] - p0[1]
+    c2 = p2[2] - p0[2]
+
+    print(a1, b1, c1)
+    print(a2, b2, c2)
+    a = b1 * c2 - b2 * c1
+    b = a2 * c1 - a1 * c2
+    c = a1 * b2 - b1 * a2
+    d = -a * p0[0] - b * p0[1] - c * p0[2]
+
+    return (a, b, c, d)
+
+
 # Part one
 # lines = [extract_line(hailstone) for hailstone in hailstones]
 # total = 0
@@ -76,3 +106,5 @@ def extract_intersection(line1, line2):
 # print(total)
 
 # Part two
+for hailstone in hailstones:
+    print(extract_plane(hailstone))

185b7bb7037ea6e8531d5fcff0c0be32d1260123
Yue Peng <yuepaang@gmail.com>
2023-12-28 01:02:49 +0800

    wip day25

--- 2023/day25/snow.py
+++ 2023/day25/snow.py
@@ -2,8 +2,8 @@ from copy import deepcopy
 from itertools import combinations
 
 
-# with open("./snow_input.txt", "r") as f:
-with open("./test.txt", "r") as f:
+# with open("./test.txt", "r") as f:
+with open("./snow_input.txt", "r") as f:
     lines = f.readlines()
 
 
@@ -41,33 +41,47 @@ for name, node in node_map.items():
 # node_map["jqt"].children.remove("nvd")
 
 
-def dfs(node, visited):
+def dfs(node, visited, nm):
     if node.name in visited:
         return
     visited.add(node.name)
     for child in node.children:
-        dfs(node_map[child], visited)
+        dfs(nm[child], visited, nm)
 
 
-group = set()
-for name, node in node_map.items():
-    visited = set()
-    dfs(node, visited)
-    group.add(tuple(sorted(visited)))
-print(group)
+# group = set()
+# for name, node in node_map.items():
+#     visited = set()
+#     dfs(node, visited, node_map)
+#     group.add(tuple(sorted(visited)))
+# print(group)
 
 all_nodes = list(node_map.keys())
 coms = combinations(all_nodes, 3)
 for com in coms:
-    tmp_node_map = deepcopy(node_map)
-    print(com)
     cand = [[] for _ in range(3)]
     for i, n in enumerate(com):
-        for on in tmp_node_map[n].children:
+        for on in node_map[n].children:
             if on in com:
                 continue
             cand[i].append((n, on))
-    print(cand)
-    for a, b, c in zip(cand[0], cand[1], cand[2]):
-        print(a, b, c)
-    break
+    for a in cand[0]:
+        for b in cand[1]:
+            for c in cand[2]:
+                tmp_node_map = deepcopy(node_map)
+                tmp_node_map[a[0]].children.remove(a[1])
+                tmp_node_map[a[1]].children.remove(a[0])
+                tmp_node_map[b[0]].children.remove(b[1])
+                tmp_node_map[b[1]].children.remove(b[0])
+                tmp_node_map[c[0]].children.remove(c[1])
+                tmp_node_map[c[1]].children.remove(c[0])
+                group = set()
+                for name, node in tmp_node_map.items():
+                    visited = set()
+                    dfs(node, visited, tmp_node_map)
+                    group.add(tuple(sorted(visited)))
+                group = list(group)
+                if len(group) == 2:
+                    print(group)
+                    print(len(group[0]) * len(group[1]))
+                    raise Exception("find it")
